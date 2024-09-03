35c8b5dcfd5acbd59a29b74e704d74b524e6c672
Maja Panoska <panoskamaja@live.com>
2024-09-03 21:46:16 +0200

    Initial commit

---
+++ src/main/java/com/example/quiziverse/QuiziverseApplication.java
@@ -0,0 +1,13 @@
+package com.example.quiziverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuiziverseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuiziverseApplication.class, args);
+    }
+
+}
